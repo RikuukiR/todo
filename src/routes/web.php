2e881058075592c62a4523a5a989d6bfec95660b
Riku <rikusugawara.1103@gamil.com>
2024-09-10 22:15:50 +0900

    upgrade

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,20 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', [TodoController::class, 'index'] );
-Route::post('/todos', [TodoController::class, 'store'] );
+Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+Route::get('/categories', [CategoryController::class, 'index']);
