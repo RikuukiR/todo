d1df5a176d53beff39e2a335bb3438d542a5d1c2
Riku <rikusugawara.1103@gamil.com>
2025-03-23 10:44:04 +0900

    3-

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -30,8 +30,10 @@
         @csrf
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content" value="{{ old('content') }}" />
-            <select name="" id="" class="create-form__item-select">
-                <option value="">カテゴリ</option>
+            <select name="category_id" id="" class="create-form__item-select">
+                @foreach ($categories as $category)
+                <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                @endforeach
             </select>
         </div>
         <div class="create-form__button">
@@ -41,11 +43,14 @@
     <div class="section__title">
         <h2>Todo検索</h2>
     </div>
-    <form action="" method="" class="search-form">
+    <form action="/todos/search" method="GET" class="search-form">
         <div class="search-form__item">
-            <input class="search-form__item-input" type="text" />
-            <select name="" id="" class="search-form__item-select">
+            <input class="search-form__item-input" type="text" name="keyword" value="{{ old('keyword') }}" />
+            <select name="category_id" id="" class="search-form__item-select">
                 <option value="">カテゴリ</option>
+                @foreach ($categories as $category)
+                <option value="{{ $category['id'] }}">{{ $category['name'] }}</option>
+                @endforeach
             </select>
         </div>
         <div class="search-form__button">
@@ -73,7 +78,7 @@
                             <input type="hidden" name="id" value="{{ $todo['id'] }}">
                         </div>
                         <div class="update-form__item">
-                            <p class="update-form__item-p">Category 1</p>
+                            <p class="update-form__item-p">{{ $todo['category']['name'] }}</p>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -95,4 +100,4 @@
         </table>
     </div>
 </div>
-@endsection('content')
+@endsection
